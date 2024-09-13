b19da4a12ff759ffcff28c4962e634b3ca9182b9
Harshavardhan Amara <harsha.prosper14@gmail.com>
2024-09-13 12:30:06 +0530

    commiting program to find unique values in list

--- src/29082024/get_unique_fromLists.py
+++ src/29082024/get_unique_fromLists.py
@@ -1,8 +1,11 @@
-original_list=[1,2,2,4,4,9,2,1,0,2,11]
+original_list=[1,2,2,4]
 unique_list=[]
 duplicate_counter=2
 for a in original_list:
     freq=original_list.count(a)
     if freq==1 and a not in unique_list:
         unique_list.append(a)
-print(unique_list)
+#print(unique_list)
+print(len(unique_list))
+for b in unique_list:
+    print(b,',')
